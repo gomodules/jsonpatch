fa431c26804d5bfa3788e8a8b1f749ae80c1cebf
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-08-09 16:41:19 +0900

    Use bytes.Equal
    Signed-off-by: Kenjiro Nakayama <nakayamakenjiro@gmail.com>

--- v2/jsonpatch.go
+++ v2/jsonpatch.go
@@ -1,6 +1,7 @@
 package jsonpatch
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"reflect"
@@ -64,7 +65,7 @@ func NewOperation(op, path string, value interface{}) Operation {
 //
 // An error will be returned if any of the two documents are invalid.
 func CreatePatch(a, b []byte) ([]Operation, error) {
-	if reflect.DeepEqual(a, b) {
+	if bytes.Equal(a, b) {
 		return []Operation{}, nil
 	}
 	var aI interface{}
